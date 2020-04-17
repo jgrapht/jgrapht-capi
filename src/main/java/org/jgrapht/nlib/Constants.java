4ef044ad6b0a2089081e12988f38a117cf35a5c5
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-04-17 11:14:35 +0300

    Better naming

--- src/main/java/org/jgrapht/nlib/Constants.java
+++ src/main/java/org/jgrapht/nlib/Constants.java
@@ -6,7 +6,7 @@ package org.jgrapht.nlib;
  */
 public class Constants {
 
-	public static final String LIB_PREFIX = "jgrapht_";
+	public static final String LIB_PREFIX = "jgrapht_nlib_";
 	public static final long LONG_NO_RESULT = -1L;
 
 }
