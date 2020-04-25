3d8ac11297afe0b5b9cab5b6e4f6129fa86b3d7e
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-04-25 21:48:40 +0300

    Added spanner API

---
+++ jgrapht-capi/src/main/java/org/jgrapht/capi/impl/SpannerApi.java
@@ -0,0 +1,55 @@
+/*
+ * (C) Copyright 2020, by Dimitrios Michail.
+ *
+ * JGraphT C-API
+ *
+ * See the CONTRIBUTORS.md file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the
+ * GNU Lesser General Public License v2.1 or later
+ * which is available at
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1-standalone.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR LGPL-2.1-or-later
+ */
+package org.jgrapht.capi.impl;
+
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.ObjectHandle;
+import org.graalvm.nativeimage.ObjectHandles;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CDoublePointer;
+import org.graalvm.nativeimage.c.type.WordPointer;
+import org.jgrapht.Graph;
+import org.jgrapht.alg.interfaces.SpannerAlgorithm;
+import org.jgrapht.alg.interfaces.SpannerAlgorithm.Spanner;
+import org.jgrapht.alg.spanning.GreedyMultiplicativeSpanner;
+import org.jgrapht.capi.Constants;
+import org.jgrapht.capi.Status;
+import org.jgrapht.capi.error.StatusReturnExceptionHandler;
+
+public class SpannerApi {
+
+	private static ObjectHandles globalHandles = ObjectHandles.getGlobal();
+
+	@CEntryPoint(name = Constants.LIB_PREFIX
+			+ "spanner_exec_greedy_multiplicative", exceptionHandler = StatusReturnExceptionHandler.class)
+	public static int executeGreedyMultiplicativeSpanner(IsolateThread thread, ObjectHandle graph, int k,
+			CDoublePointer weightRes, WordPointer res) {
+		Graph<Long, Long> g = globalHandles.get(graph);
+		SpannerAlgorithm<Long> spannerAlg = new GreedyMultiplicativeSpanner<>(g, k);
+		Spanner<Long> spanner = spannerAlg.getSpanner();
+		double weight = spanner.getWeight();
+		if (weightRes.isNonNull()) {
+			weightRes.write(weight);
+		}
+		if (res.isNonNull()) {
+			res.write(globalHandles.create(spanner));
+		}
+		return Status.SUCCESS.toCEnum();
+	}
+
+}
---
+++ test/test_spanner.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <jgrapht_capi.h>
+
+#define ITERATOR_NO_SUCH_ELEMENT 100
+
+int main() {
+    graal_isolate_t *isolate = NULL;
+    graal_isolatethread_t *thread = NULL;
+
+    if (thread, graal_create_isolate(NULL, &isolate, &thread) != 0) {
+        fprintf(stderr, "graal_create_isolate error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    assert(jgrapht_capi_get_errno(thread) == 0);
+
+    void *g;
+    jgrapht_capi_graph_create(thread, 0, 0, 0, 0, &g);
+    assert(jgrapht_capi_get_errno(thread) == 0);
+
+    int flag;
+    assert(jgrapht_capi_graph_is_directed(thread, g, &flag) == 0);
+    assert(flag == 0);
+    assert(jgrapht_capi_graph_is_undirected(thread, g, &flag) == 0);
+    assert(flag == 1);
+    assert(jgrapht_capi_graph_is_weighted(thread, g, &flag) == 0);
+    assert(flag == 0);
+    assert(jgrapht_capi_graph_is_allowing_selfloops(thread, g, &flag) == 0);
+    assert(flag == 0);
+    assert(jgrapht_capi_graph_is_allowing_multipleedges(thread, g, &flag) == 0);
+    assert(flag == 0);
+
+    long long v;
+    long long e;
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+
+    jgrapht_capi_graph_add_edge(thread, g, 0, 1, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 1, 2, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 2, 3, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 3, 4, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 4, 0, NULL);
+
+    jgrapht_capi_graph_set_edge_weight(thread, g, 0, 5.0);
+    jgrapht_capi_graph_set_edge_weight(thread, g, 1, 4.0);
+    jgrapht_capi_graph_set_edge_weight(thread, g, 2, 3.0);
+    jgrapht_capi_graph_set_edge_weight(thread, g, 3, 2.0);
+    jgrapht_capi_graph_set_edge_weight(thread, g, 4, 1.0);
+
+    // run 
+    void *spanner;
+    double weight;
+    assert(jgrapht_capi_spanner_exec_greedy_multiplicative(thread, g, 3, &weight, &spanner)==0);
+    assert(weight == 4.0);
+    long long size;
+    jgrapht_capi_set_size(thread, spanner, &size);
+    assert(size == 4);
+    int contains;
+    jgrapht_capi_set_long_contains(thread, spanner, 0, &contains);
+    assert(contains);
+    jgrapht_capi_set_long_contains(thread, spanner, 1, &contains);
+    assert(contains);
+    jgrapht_capi_set_long_contains(thread, spanner, 2, &contains);
+    assert(contains);
+    jgrapht_capi_set_long_contains(thread, spanner, 3, &contains);
+    assert(contains);
+    jgrapht_capi_set_long_contains(thread, spanner, 4, &contains);
+    assert(!contains);
+    jgrapht_capi_destroy(thread,  spanner);
+
+    jgrapht_capi_destroy(thread, g);
+
+    if (thread, graal_detach_thread(thread) != 0) {
+        fprintf(stderr, "graal_detach_thread error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
+}
