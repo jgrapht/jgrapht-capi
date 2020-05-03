879f62be285e284fe259f5983ac8ef430d18f983
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-05-03 22:16:31 +0300

    Added planarity api

---
+++ jgrapht-capi/src/main/java/org/jgrapht/capi/impl/PlanarityApi.java
@@ -0,0 +1,59 @@
+package org.jgrapht.capi.impl;
+
+import java.util.List;
+
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.ObjectHandle;
+import org.graalvm.nativeimage.ObjectHandles;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CIntPointer;
+import org.graalvm.nativeimage.c.type.WordPointer;
+import org.jgrapht.Graph;
+import org.jgrapht.alg.interfaces.PlanarityTestingAlgorithm.Embedding;
+import org.jgrapht.alg.planar.BoyerMyrvoldPlanarityInspector;
+import org.jgrapht.capi.Constants;
+import org.jgrapht.capi.JGraphTContext.Status;
+import org.jgrapht.capi.error.StatusReturnExceptionHandler;
+
+public class PlanarityApi {
+
+	private static ObjectHandles globalHandles = ObjectHandles.getGlobal();
+
+	@CEntryPoint(name = Constants.LIB_PREFIX
+			+ "planarity_exec_boyer_myrvold", exceptionHandler = StatusReturnExceptionHandler.class)
+	public static int executeBoryerMyrvold(IsolateThread thread, ObjectHandle graphHandle, CIntPointer res,
+			WordPointer embeddingRes, WordPointer kuratowskiSubdivisionRes) {
+		Graph<Long, Long> g = globalHandles.get(graphHandle);
+
+		BoyerMyrvoldPlanarityInspector<Long, Long> alg = new BoyerMyrvoldPlanarityInspector<>(g);
+
+		boolean result = alg.isPlanar();
+		if (res.isNonNull()) {
+			res.write(result ? 1 : 0);
+		}
+		if (result) {
+			Embedding<Long, Long> embedding = alg.getEmbedding();
+			if (embeddingRes.isNonNull()) {
+				embeddingRes.write(globalHandles.create(embedding));
+			}
+		} else {
+			Graph<Long, Long> kuratowskiSubdivision = alg.getKuratowskiSubdivision();
+			if (kuratowskiSubdivisionRes.isNonNull()) {
+				kuratowskiSubdivisionRes.write(globalHandles.create(kuratowskiSubdivision));
+			}
+		}
+		return Status.STATUS_SUCCESS.getCValue();
+	}
+
+	@CEntryPoint(name = Constants.LIB_PREFIX
+			+ "planarity_embedding_edges_around_vertex", exceptionHandler = StatusReturnExceptionHandler.class)
+	public static int edgesAround(IsolateThread thread, ObjectHandle embeddingHandle, long vertex, WordPointer res) {
+		Embedding<Long, Long> embedding = globalHandles.get(embeddingHandle);
+		List<Long> list = embedding.getEdgesAround(vertex);
+		if (list != null && res.isNonNull()) {
+			res.write(globalHandles.create(list.iterator()));
+		}
+		return Status.STATUS_SUCCESS.getCValue();
+	}
+
+}
---
+++ test/test_planar.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <jgrapht_capi_types.h>
+#include <jgrapht_capi.h>
+
+
+int main() {
+    graal_isolate_t *isolate = NULL;
+    graal_isolatethread_t *thread = NULL;
+
+    if (thread, graal_create_isolate(NULL, &isolate, &thread) != 0) {
+        fprintf(stderr, "graal_create_isolate error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    assert(jgrapht_capi_get_errno(thread) == 0);
+
+    void *g;
+    jgrapht_capi_graph_create(thread, 0, 0, 0, 0, &g);
+    assert(jgrapht_capi_get_errno(thread) == 0);
+
+
+    long long v;
+    long long e;
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+
+    jgrapht_capi_graph_add_edge(thread, g, 0, 1, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 1, 2, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 2, 3, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 3, 4, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 4, 0, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 2, 0, NULL);
+
+    void *embedding, *kuratowski_subdivision;
+    int is_planar = 0;
+
+    jgrapht_capi_planarity_exec_boyer_myrvold(thread, g, &is_planar, &embedding, &kuratowski_subdivision);
+    assert (is_planar);
+
+    void *it;
+    jgrapht_capi_planarity_embedding_edges_around_vertex(thread, embedding, 0, &it);
+
+    jgrapht_capi_it_next_long(thread, it, &e);
+    assert (e == 5);
+    jgrapht_capi_it_next_long(thread, it, &e);
+    assert (e == 0);
+    jgrapht_capi_it_next_long(thread, it, &e);
+    assert (e == 4);
+    int has_next = 1;
+    jgrapht_capi_it_hasnext(thread, it, &has_next);
+    assert (has_next == 0);
+
+    assert(jgrapht_capi_get_errno(thread) == 0);
+
+    jgrapht_capi_destroy(thread, it);
+    jgrapht_capi_destroy(thread, embedding);
+    jgrapht_capi_destroy(thread, g);
+
+    assert(jgrapht_capi_get_errno(thread) == 0);
+
+    if (thread, graal_detach_thread(thread) != 0) {
+        fprintf(stderr, "graal_detach_thread error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
+}
