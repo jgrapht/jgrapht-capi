7e3e50628e3010000f00f9b1bbd6962798441be9
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-05-08 20:36:56 +0300

    Added api for isomorphism

---
+++ jgrapht-capi/src/main/java/org/jgrapht/capi/impl/IsomorphismApi.java
@@ -0,0 +1,105 @@
+package org.jgrapht.capi.impl;
+
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.ObjectHandle;
+import org.graalvm.nativeimage.ObjectHandles;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CIntPointer;
+import org.graalvm.nativeimage.c.type.CLongPointer;
+import org.graalvm.nativeimage.c.type.WordPointer;
+import org.jgrapht.Graph;
+import org.jgrapht.GraphMapping;
+import org.jgrapht.alg.isomorphism.VF2GraphIsomorphismInspector;
+import org.jgrapht.alg.isomorphism.VF2SubgraphIsomorphismInspector;
+import org.jgrapht.capi.Constants;
+import org.jgrapht.capi.JGraphTContext.Status;
+import org.jgrapht.capi.error.StatusReturnExceptionHandler;
+
+public class IsomorphismApi {
+
+	private static ObjectHandles globalHandles = ObjectHandles.getGlobal();
+
+	@CEntryPoint(name = Constants.LIB_PREFIX
+			+ "isomorphism_exec_vf2", exceptionHandler = StatusReturnExceptionHandler.class)
+	public static int executeVF2(IsolateThread thread, ObjectHandle graph1Handle, ObjectHandle graph2Handle,
+			CIntPointer existsRes, WordPointer graphMappingIteratorRes) {
+		Graph<Long, Long> g1 = globalHandles.get(graph1Handle);
+		Graph<Long, Long> g2 = globalHandles.get(graph2Handle);
+
+		VF2GraphIsomorphismInspector<Long, Long> alg = new VF2GraphIsomorphismInspector<>(g1, g2);
+		boolean exists = alg.isomorphismExists();
+		if (existsRes.isNonNull()) {
+			existsRes.write(exists ? 1 : 0);
+		}
+		if (exists && graphMappingIteratorRes.isNonNull()) {
+			graphMappingIteratorRes.write(globalHandles.create(alg.getMappings()));
+		}
+		return Status.STATUS_SUCCESS.getCValue();
+	}
+
+	/**
+	 * Note that this inspector only finds isomorphisms between a smaller graph and
+	 * all
+	 * <a href="http://mathworld.wolfram.com/Vertex-InducedSubgraph.html">induced
+	 * subgraphs</a> of a larger graph. It does not find isomorphisms between the
+	 * smaller graph and arbitrary subgraphs of the larger graph.
+	 */
+	@CEntryPoint(name = Constants.LIB_PREFIX
+			+ "isomorphism_exec_vf2_subgraph", exceptionHandler = StatusReturnExceptionHandler.class)
+	public static int executeVF2Subgraph(IsolateThread thread, ObjectHandle graph1Handle, ObjectHandle graph2Handle,
+			CIntPointer existsRes, WordPointer graphMappingIteratorRes) {
+		Graph<Long, Long> g1 = globalHandles.get(graph1Handle);
+		Graph<Long, Long> g2 = globalHandles.get(graph2Handle);
+
+		VF2SubgraphIsomorphismInspector<Long, Long> alg = new VF2SubgraphIsomorphismInspector<>(g1, g2);
+		boolean exists = alg.isomorphismExists();
+		if (existsRes.isNonNull()) {
+			existsRes.write(exists ? 1 : 0);
+		}
+		if (exists && graphMappingIteratorRes.isNonNull()) {
+			graphMappingIteratorRes.write(globalHandles.create(alg.getMappings()));
+		}
+		return Status.STATUS_SUCCESS.getCValue();
+	}
+
+	@CEntryPoint(name = Constants.LIB_PREFIX
+			+ "isomorphism_graph_mapping_edge_correspondence", exceptionHandler = StatusReturnExceptionHandler.class)
+	public static int graphMappingEdge(IsolateThread thread, ObjectHandle mappingHandle, long edge, boolean forward,
+			CIntPointer existsEdgeRes, CLongPointer edgeRes) {
+		GraphMapping<Long, Long> graphMapping = globalHandles.get(mappingHandle);
+
+		Long otherEdge = graphMapping.getEdgeCorrespondence(edge, forward);
+		if (existsEdgeRes.isNonNull()) {
+			if (otherEdge != null) {
+				existsEdgeRes.write(1);
+				if (edgeRes.isNonNull()) {
+					edgeRes.write(otherEdge);
+				}
+			} else {
+				existsEdgeRes.write(0);
+			}
+		}
+		return Status.STATUS_SUCCESS.getCValue();
+	}
+
+	@CEntryPoint(name = Constants.LIB_PREFIX
+			+ "isomorphism_graph_mapping_vertexe_correspondence", exceptionHandler = StatusReturnExceptionHandler.class)
+	public static int graphMappingVertex(IsolateThread thread, ObjectHandle mappingHandle, long vertex, boolean forward,
+			CIntPointer existsVertexRes, CLongPointer vertexRes) {
+		GraphMapping<Long, Long> graphMapping = globalHandles.get(mappingHandle);
+
+		Long otherVertex = graphMapping.getVertexCorrespondence(vertex, forward);
+		if (existsVertexRes.isNonNull()) {
+			if (otherVertex != null) {
+				existsVertexRes.write(1);
+				if (vertexRes.isNonNull()) {
+					vertexRes.write(otherVertex);
+				}
+			} else {
+				existsVertexRes.write(0);
+			}
+		}
+		return Status.STATUS_SUCCESS.getCValue();
+	}
+
+}
---
+++ test/test_iso_subgraph.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <jgrapht_capi_types.h>
+#include <jgrapht_capi.h>
+
+
+int main() {
+    graal_isolate_t *isolate = NULL;
+    graal_isolatethread_t *thread = NULL;
+
+    if (thread, graal_create_isolate(NULL, &isolate, &thread) != 0) {
+        fprintf(stderr, "graal_create_isolate error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    void *g1;
+    jgrapht_capi_graph_create(thread, 0, 0, 0, 1, &g1);
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    long long v;
+    long long e;
+    jgrapht_capi_graph_add_vertex(thread, g1, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g1, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g1, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g1, NULL);
+
+    jgrapht_capi_graph_add_edge(thread, g1, 0, 1, NULL);
+    jgrapht_capi_graph_add_edge(thread, g1, 1, 2, NULL);
+    jgrapht_capi_graph_add_edge(thread, g1, 2, 3, NULL);
+    jgrapht_capi_graph_add_edge(thread, g1, 3, 0, NULL);
+
+    void *g2;
+    jgrapht_capi_graph_create(thread, 0, 0, 0, 1, &g2);
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    jgrapht_capi_graph_add_vertex(thread, g2, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g2, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g2, NULL);
+
+    jgrapht_capi_graph_add_edge(thread, g2, 0, 1, NULL);
+    jgrapht_capi_graph_add_edge(thread, g2, 1, 2, NULL);
+
+    int are_iso = 0;
+    void *map_it;
+    jgrapht_capi_isomorphism_exec_vf2_subgraph(thread, g1, g2, &are_iso, &map_it);
+    jgrapht_capi_error_print_stack_trace(thread);
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    assert(are_iso);
+
+    void *map;
+    jgrapht_capi_it_next_object(thread, map_it, &map);
+
+    int has_other_vertex;
+    long long other_vertex;
+    jgrapht_capi_isomorphism_graph_mapping_vertexe_correspondence(thread, map, 0, 1, &has_other_vertex, &other_vertex);
+    assert(has_other_vertex && other_vertex == 0);
+    jgrapht_capi_isomorphism_graph_mapping_vertexe_correspondence(thread, map, 1, 1, &has_other_vertex, &other_vertex);
+    assert(has_other_vertex && other_vertex == 1);
+    jgrapht_capi_isomorphism_graph_mapping_vertexe_correspondence(thread, map, 2, 1, &has_other_vertex, &other_vertex);
+    assert(has_other_vertex && other_vertex == 2);
+    jgrapht_capi_isomorphism_graph_mapping_vertexe_correspondence(thread, map, 3, 1, &has_other_vertex, &other_vertex);
+    assert(!has_other_vertex);
+    jgrapht_capi_handles_destroy(thread, map);
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    // next mapping
+    jgrapht_capi_it_next_object(thread, map_it, &map);
+    jgrapht_capi_isomorphism_graph_mapping_vertexe_correspondence(thread, map, 0, 1, &has_other_vertex, &other_vertex);
+    assert(has_other_vertex && other_vertex == 0);
+    jgrapht_capi_isomorphism_graph_mapping_vertexe_correspondence(thread, map, 1, 1, &has_other_vertex, &other_vertex);
+    assert(!has_other_vertex);
+    jgrapht_capi_isomorphism_graph_mapping_vertexe_correspondence(thread, map, 2, 1, &has_other_vertex, &other_vertex);
+    assert(has_other_vertex && other_vertex == 2);
+    jgrapht_capi_isomorphism_graph_mapping_vertexe_correspondence(thread, map, 3, 1, &has_other_vertex, &other_vertex);
+    assert(has_other_vertex && other_vertex == 1);
+    jgrapht_capi_handles_destroy(thread, map);
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    int has_next = 0;
+    jgrapht_capi_it_hasnext(thread, map_it, &has_next);
+    assert(has_next == 1);
+
+    jgrapht_capi_handles_destroy(thread, map_it);
+    jgrapht_capi_handles_destroy(thread, g1);
+    jgrapht_capi_handles_destroy(thread, g2);
+
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+    
+    if (thread, graal_detach_thread(thread) != 0) {
+        fprintf(stderr, "graal_detach_thread error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
+}
