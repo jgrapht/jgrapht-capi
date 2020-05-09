ea7b58d8cd3bdc0e0064875bdbba54a810de8fbf
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-05-09 13:01:57 +0300

    Added tests for eulerian and chinese postman

---
+++ test/test_chinese_postman.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <jgrapht_capi_types.h>
+#include <jgrapht_capi.h>
+
+
+int main() {
+    graal_isolate_t *isolate = NULL;
+    graal_isolatethread_t *thread = NULL;
+
+    if (thread, graal_create_isolate(NULL, &isolate, &thread) != 0) {
+        fprintf(stderr, "graal_create_isolate error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    void *g;
+    jgrapht_capi_graph_create(thread, 0, 0, 0, 1, &g);
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    long long v;
+    long long e;
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+
+    jgrapht_capi_graph_add_edge(thread, g, 0, 1, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 1, 2, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 2, 3, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 3, 0, NULL);
+
+    jgrapht_capi_graph_add_edge(thread, g, 3, 4, NULL);
+
+    void *walk;
+    int exists;
+    jgrapht_capi_cycles_chinese_postman_exec_edmonds_johnson(thread, g, &walk);
+
+    double weight;
+    long long int start_vertex;
+    long long int end_vertex;
+
+    jgrapht_capi_graphpath_get_fields(thread, walk, &weight, &start_vertex, &end_vertex, NULL);
+
+    assert(weight == 6.0);
+
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    jgrapht_capi_handles_destroy(thread, walk);
+    jgrapht_capi_handles_destroy(thread, g);
+
+    if (thread, graal_detach_thread(thread) != 0) {
+        fprintf(stderr, "graal_detach_thread error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ test/test_eulerian.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <jgrapht_capi_types.h>
+#include <jgrapht_capi.h>
+
+
+int main() {
+    graal_isolate_t *isolate = NULL;
+    graal_isolatethread_t *thread = NULL;
+
+    if (thread, graal_create_isolate(NULL, &isolate, &thread) != 0) {
+        fprintf(stderr, "graal_create_isolate error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    void *g;
+    jgrapht_capi_graph_create(thread, 0, 0, 0, 1, &g);
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    long long v;
+    long long e;
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+
+    jgrapht_capi_graph_add_edge(thread, g, 0, 1, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 1, 2, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 2, 3, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 3, 0, NULL);
+
+    void *cycle;
+    int exists;
+    jgrapht_capi_cycles_eulerian_exec_hierholzer(thread, g, &exists, &cycle);
+
+    assert(exists);
+
+    jgrapht_capi_handles_destroy(thread, cycle);
+
+    jgrapht_capi_handles_destroy(thread, g);
+
+    if (thread, graal_detach_thread(thread) != 0) {
+        fprintf(stderr, "graal_detach_thread error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
+}
