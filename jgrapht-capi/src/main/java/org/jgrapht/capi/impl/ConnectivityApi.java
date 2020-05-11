662ddf8de9e0ca85a927a40eeef8b5280a8c6952
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-05-11 09:36:38 +0300

    Added API for connectivity

---
+++ jgrapht-capi/src/main/java/org/jgrapht/capi/impl/ConnectivityApi.java
@@ -0,0 +1,84 @@
+package org.jgrapht.capi.impl;
+
+import java.util.List;
+import java.util.Set;
+
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.ObjectHandle;
+import org.graalvm.nativeimage.ObjectHandles;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CIntPointer;
+import org.graalvm.nativeimage.c.type.WordPointer;
+import org.jgrapht.Graph;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
+import org.jgrapht.alg.connectivity.GabowStrongConnectivityInspector;
+import org.jgrapht.alg.connectivity.KosarajuStrongConnectivityInspector;
+import org.jgrapht.capi.Constants;
+import org.jgrapht.capi.JGraphTContext.Status;
+import org.jgrapht.capi.error.StatusReturnExceptionHandler;
+
+public class ConnectivityApi {
+
+	private static ObjectHandles globalHandles = ObjectHandles.getGlobal();
+
+	@CEntryPoint(name = Constants.LIB_PREFIX
+			+ "connectivity_strong_exec_kosaraju", exceptionHandler = StatusReturnExceptionHandler.class)
+	public static int executeKosaraju(IsolateThread thread, ObjectHandle graphHandle, CIntPointer valueRes,
+			WordPointer res) {
+		Graph<Long, Long> g = globalHandles.get(graphHandle);
+
+		KosarajuStrongConnectivityInspector<Long, Long> alg = new KosarajuStrongConnectivityInspector<>(g);
+
+		boolean result = alg.isStronglyConnected();
+		List<Set<Long>> connectedSets = alg.stronglyConnectedSets();
+
+		if (valueRes.isNonNull()) {
+			valueRes.write(result ? 1 : 0);
+		}
+		if (res.isNonNull()) {
+			res.write(globalHandles.create(connectedSets.iterator()));
+		}
+		return Status.STATUS_SUCCESS.getCValue();
+	}
+
+	@CEntryPoint(name = Constants.LIB_PREFIX
+			+ "connectivity_strong_exec_gabow", exceptionHandler = StatusReturnExceptionHandler.class)
+	public static int executeGabow(IsolateThread thread, ObjectHandle graphHandle, CIntPointer valueRes,
+			WordPointer res) {
+		Graph<Long, Long> g = globalHandles.get(graphHandle);
+
+		GabowStrongConnectivityInspector<Long, Long> alg = new GabowStrongConnectivityInspector<>(g);
+
+		boolean result = alg.isStronglyConnected();
+		List<Set<Long>> connectedSets = alg.stronglyConnectedSets();
+
+		if (valueRes.isNonNull()) {
+			valueRes.write(result ? 1 : 0);
+		}
+		if (res.isNonNull()) {
+			res.write(globalHandles.create(connectedSets.iterator()));
+		}
+		return Status.STATUS_SUCCESS.getCValue();
+	}
+
+	@CEntryPoint(name = Constants.LIB_PREFIX
+			+ "connectivity_weak_exec_bfs", exceptionHandler = StatusReturnExceptionHandler.class)
+	public static int executeWeakBfs(IsolateThread thread, ObjectHandle graphHandle, CIntPointer valueRes,
+			WordPointer res) {
+		Graph<Long, Long> g = globalHandles.get(graphHandle);
+
+		ConnectivityInspector<Long, Long> alg = new ConnectivityInspector<>(g);
+
+		boolean result = alg.isConnected();
+		List<Set<Long>> connectedSets = alg.connectedSets();
+
+		if (valueRes.isNonNull()) {
+			valueRes.write(result ? 1 : 0);
+		}
+		if (res.isNonNull()) {
+			res.write(globalHandles.create(connectedSets.iterator()));
+		}
+		return Status.STATUS_SUCCESS.getCValue();
+	}
+
+}
---
+++ test/test_connectivity.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include <jgrapht_capi_types.h>
+#include <jgrapht_capi.h>
+
+
+int main() {
+    graal_isolate_t *isolate = NULL;
+    graal_isolatethread_t *thread = NULL;
+
+    if (thread, graal_create_isolate(NULL, &isolate, &thread) != 0) {
+        fprintf(stderr, "graal_create_isolate error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    void *g;
+    jgrapht_capi_graph_create(thread, 1, 0, 0, 1, &g);
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    long long v;
+    long long e;
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+    jgrapht_capi_graph_add_vertex(thread, g, NULL);
+
+    jgrapht_capi_graph_add_edge(thread, g, 0, 1, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 1, 2, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 2, 0, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 3, 0, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 3, 4, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 4, 5, NULL);
+    jgrapht_capi_graph_add_edge(thread, g, 5, 3, NULL);
+
+    int is_connected;
+    void *setsit;
+    jgrapht_capi_connectivity_weak_exec_bfs(thread, g, &is_connected, &setsit);
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    assert(is_connected == 1);
+
+    void *set;
+    jgrapht_capi_it_next_object(thread, setsit, &set);
+
+    long long size = 0;
+    jgrapht_capi_set_size(thread, set, &size);
+    assert(size == 6);
+    jgrapht_capi_handles_destroy(thread, set);
+    jgrapht_capi_handles_destroy(thread, setsit);
+
+
+    // gabow
+    jgrapht_capi_connectivity_strong_exec_gabow(thread, g, &is_connected, NULL);
+    assert(!is_connected);
+
+    // kosaraju
+    jgrapht_capi_connectivity_strong_exec_kosaraju(thread, g, &is_connected, NULL);
+    assert(!is_connected);
+
+    // make strongly connected
+    jgrapht_capi_graph_add_edge(thread, g, 0, 3, NULL);
+
+    // gabow
+    jgrapht_capi_connectivity_strong_exec_gabow(thread, g, &is_connected, NULL);
+    assert(is_connected);
+
+    // kosaraju
+    jgrapht_capi_connectivity_strong_exec_kosaraju(thread, g, &is_connected, NULL);
+    assert(is_connected);
+
+    jgrapht_capi_handles_destroy(thread, g);
+
+    assert(jgrapht_capi_error_get_errno(thread) == 0);
+
+    if (thread, graal_detach_thread(thread) != 0) {
+        fprintf(stderr, "graal_detach_thread error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
+}
