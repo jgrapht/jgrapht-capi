c78ff191dc16f1905c92f9ee4723bbb683012bee
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-06-07 10:35:22 +0300

    Cleaned up as weighted graph due to merge in upstream

--- jgrapht-capi/src/main/java/org/jgrapht/capi/graph/CapiAsWeightedGraph.java
+++ jgrapht-capi/src/main/java/org/jgrapht/capi/graph/CapiAsWeightedGraph.java
@@ -10,25 +10,12 @@ public class CapiAsWeightedGraph extends AsWeightedGraph<Integer, Integer>
 	private static final long serialVersionUID = 1L;
 
 	private Graph<Integer, Integer> wrappedGraph;
-	private boolean superHasWeightFunction;
-	private boolean superHasCacheWeights;
 
 	public CapiAsWeightedGraph(Graph<Integer, Integer> g, Function<Integer, Double> weightFunction, boolean cacheWeights,
 	        boolean writeWeightsThrough) {
 		super(g, weightFunction, cacheWeights, writeWeightsThrough);
-		this.superHasCacheWeights = cacheWeights;
-		this.superHasWeightFunction = weightFunction != null;
 		this.wrappedGraph = g;
 	}
-
-	@Override
-    public void setEdgeWeight(Integer e, double weight)
-    {
-		if (superHasWeightFunction && !superHasCacheWeights) { 
-			throw new UnsupportedOperationException("Cannot set an edge weight when a weight function is used and caching is disabled");
-		}
-		super.setEdgeWeight(e, weight);
-    }
 	
 	@Override
 	public Graph<Integer, Integer> getWrappedGraph() {
